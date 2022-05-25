790001cae66c7b48f383875b6dff2f3ba0c78543
dmitrymack <dmitrymack12@gmail.com>
2022-05-25 20:33:13 +0700

    Add panel with exchange rate

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', view.main_view, name='main'),
     path('stock/<str:ticker>', view.stock_view, name='stock'),
     path('get_info/<str:ticker>', view.get_info, name='info'),
+    path('get_courses', view.get_cur_courses, name='courses'),
     path('fillbase', view.fill_base),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from urllib.request import urlopen
+import json
 from .func import get_stock_info, filter_stocks
 from .models import Stock
 
@@ -7,6 +9,17 @@ from .models import Stock
 def get_info(request, ticker):
     response = get_stock_info(ticker)
     return JsonResponse(response)
+
+
+def get_cur_courses(request):
+    resp = urlopen('https://www.cbr-xml-daily.ru/daily_json.js')
+    dat = json.loads(resp.read())
+    dat['Valute']['USD']['Value'] = round(dat['Valute']['USD']['Value'], 2)
+    dat['Valute']['EUR']['Value'] = round(dat['Valute']['EUR']['Value'], 2)
+    dat['Valute']['GBP']['Value'] = round(dat['Valute']['GBP']['Value'], 2)
+    dat['Valute']['CNY']['Value'] = round(dat['Valute']['CNY']['Value'] / 10, 2)
+    dat['Valute']['JPY']['Value'] = round(dat['Valute']['JPY']['Value'] / 100, 2)
+    return JsonResponse(dat['Valute'])
 
 
 def fill_base(request):
@@ -26,7 +39,7 @@ def stock_view(request, ticker):
     return render(request, "main/stock.html", context={
         "ticker": ticker,
         "price": dat['price']['regularMarketPrice']['raw'],
-        "name": dat['price']['longName'],
+        "name": dat['price']['shortName'],
         "cur": dat['price']['currency']
     })
 
